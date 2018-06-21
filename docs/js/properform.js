2a4972aec0456520c6acd78d2c76f16068b7af24
joungtao <joungtao@gmail.com>
2018-06-21 21:23:05 +0800

    click statistic

--- docs/js/properform.js
+++ docs/js/properform.js
@@ -1,5 +1,5 @@
 function loadProjectStatistics(path) {
-	$.getJSON(path + "/statistics.json", function(data) {
+	$.getJSON(path + "/statistics.json", data => {
 		var ds = new DataSet();
 		var dv = ds.createView().source(data);
 		dv.transform({
@@ -24,7 +24,7 @@ function loadProjectStatistics(path) {
 		});
 		chart.axis('timestamp', {
 			label: {
-				formatter: function(val) {
+				formatter: val => {
 					if (val.substring(val.length - 9) == ' 00:00:00') {
 						return val.substring(0, val.length - 9);
 					}
@@ -42,43 +42,38 @@ function loadProjectStatistics(path) {
 			stroke: '#fff',
 			lineWidth: 1
 		});
+
+		chart.on('click', ev => {
+			var items = chart.getSnapRecords(ev);
+			if (items.length > 0) {
+				loadDiff(path, items[0]._origin.diff);
+			}
+		});
+
 		chart.render();
+		loadDiff(path, data[0].diff);
 	});
-	loadDiff(path);
 }
 
 function loadDiff(path, diff) {
-	$.getJSON(path + "/../g6-index.json", function(data) {
-		var Template = G6.Plugins['template.maxSpanningForest'];
-		var Mapper = G6.Plugins['tool.d3.mapper'];
-		var nodeSizeMapper = new Mapper('node', 'weight', 'size', [8, 20], {
-			legendCfg: null
-		});
-		var edgeSizeMapper = new Mapper('edge', 'weight', 'size', [1, 8], {
-			legendCfg: null
-		});
-		var nodeColorMapper = new Mapper('node', 'weight', 'color', ['#E0F5FF', '#BAE7FF', '#91D5FF', '#69C0FF', '#3DA0F2', '#1581E6', '#0860BF'], {
-			legendCfg: null
-		});
-		var template = new Template();
+	$.getJSON(path + "/../g6-index.json", data => {
 		var graph = new G6.Graph({
 			id: 'diffGraph', // dom id
 			height: $('#diffGraph').width() * 0.5,
-			plugins: [template, nodeSizeMapper, nodeColorMapper, edgeSizeMapper],
+			plugins: [new G6.Plugins['template.maxSpanningForest']()],
 			animate: true
 		});
-		var force = template.layout;
-		var circle = new G6.Layouts['Circle']({
-			sort: function sort(a, b) {
-				return a.weight - b.weight;
+		graph.edge({
+			style: function style(model) {
+				return {
+					stroke: graph.find(model.target).getModel().color,
+					strokeOpacity: 0.8
+				};
 			}
 		});
-		var grid = new G6.Layouts['Grid']({
-			sort: function sort(a, b) {
-				return b.weight - a.weight;
-			}
-		});
-		var dagre = new G6.Layouts['Dagre']({
+		graph.read(data);
+
+		graph.changeLayout(new G6.Layouts.Dagre({
 			nodesep: function nodesep() {
 				return graph.getWidth() / 50;
 			},
@@ -93,30 +88,22 @@ function loadDiff(path, diff) {
 			},
 
 			useEdgeControlPoint: false
-		});
-		var spiral = new G6.Layouts['ArchimeddeanSpiral']({
-			sort: function sort(a, b) {
-				return b.weight - a.weight;
-			}
-		});
-		graph.edge({
-			style: function style(model) {
-				return {
-					stroke: graph.find(model.target).getModel().color,
-					strokeOpacity: 0.8
+		}));
+
+		(graph => {
+			var lastPoint = void 0;
+			graph.on('drag', ev => {
+				if (lastPoint) {
+					graph.translate(ev.domX - lastPoint.x, ev.domY - lastPoint.y);
+				}
+				lastPoint = {
+					x: ev.domX,
+					y: ev.domY
 				};
-			}
-		});
-		graph.read(data);
-		setInterval(function() {
-			if (document.visibilityState === 'visible') {
-				var layouts = [circle, dagre, force, grid, spiral];
-				layouts = layouts.filter(function(layout) {
-					return layout !== graph.getLayout();
-				});
-				var layout = layouts[parseInt(layouts.length * Math.random())];
-				graph.changeLayout(layout);
-			}
-		}, 2000);
+			});
+			graph.on('dragend', ev => {
+				lastPoint = undefined;
+			});
+		})(graph);
 	});
 }
